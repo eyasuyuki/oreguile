70bdbd9c3d96b3e4fe786bafef6de21fa7dc1ce8
eyasuyuki <eyasuyuki@ecba4ec2-7a83-11de-9499-4f8beaa274e3>
2009-07-29 06:33:28 +0000

    add zoom controller and MyLocationOverlay

--- trunk/Oreguile/src/jp/gr/puzzle/gps/Oreguile.java
+++ trunk/Oreguile/src/jp/gr/puzzle/gps/Oreguile.java
@@ -1,13 +1,67 @@
 package jp.gr.puzzle.gps;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Oreguile extends Activity {
-    /** Called when the activity is first created. */
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class Oreguile extends MapActivity {
+	private List<Location> points = new ArrayList<Location>();
+	private FrameLayout frame = null;
+	private MapView map = null;
+	private MapController controller = null;
+	
+    @Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initViews();
+        initZoomControll();
+        initMyLocation();
+    }
+    
+    private void initViews() {
+    	frame = (FrameLayout)findViewById(R.id.frame);
+        map = (MapView)findViewById(R.id.map);
+        controller = map.getController();
+    }
+    
+    private void initZoomControll() {
+    	View zoomControl = map.getZoomControls();
+    	FrameLayout.LayoutParams p =
+    		new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+    									LayoutParams.WRAP_CONTENT,
+    									Gravity.BOTTOM + Gravity.CENTER_HORIZONTAL);
+    	frame.addView(zoomControl, p);
+    }
+    
+    private void initMyLocation() {
+    	final MyLocationOverlay overlay = new MyLocationOverlay(this, map);
+    	overlay.enableMyLocation();
+    	overlay.enableCompass();
+    	overlay.runOnFirstFix(new Runnable() {
+    		public void run() {
+    			controller.setZoom(64);
+    			controller.animateTo(overlay.getMyLocation());
+    		}
+    	});
+    	map.getOverlays().add(overlay);
     }
 }
